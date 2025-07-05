0299db1120137c3d59778dfa30b382565ddf656d
PiEtRo0o0 <103857125+PiEtRo0o0@users.noreply.github.com>
2025-07-06 00:00:14 +0100

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -3,5 +3,5 @@ var loader = document.querySelector(".loader")
 window.addEventListener("load", vanish);
 
 function vanish() {
-  loader.classList.add("disppear");
+  loader.classList.add("disappear");
 }
